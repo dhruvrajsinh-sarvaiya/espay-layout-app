ff61dc9e2eb90b27a7b2313ec22b4830fd2bd38d
dhruvrajsinh-sarvaiya <40258880+dhruvrajsinh-sarvaiya@users.noreply.github.com>
2019-06-19 20:28:57 +0530

    remove unwanted files

--- client/src/api/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-export default
-   axios.create({
-      //baseURL: 'https://6768-2901zz06.azurewebsites.net/',
-	  baseURL: 'http://172.20.65.178:5000',
-      timeout: 2000,
-      headers: {'Authorization': 'JWT ' + localStorage.getItem('front_access_token')}
-   });
