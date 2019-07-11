e4b809cbad701ab901ee9eb3e105cf8fadad2fa3
Nirmit <42736478+nirmitwaghela@users.noreply.github.com>
2019-07-11 15:14:19 +0530

    svn delete for old repo

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-export default
-   axios.create({
-      // baseURL:'http://localhost:5000', //Palak 26.06.2019
-      //baseURL: 'https://6768-2901zz06.azurewebsites.net/',
-      baseURL: 'http://172.20.65.125:7000',
-      timeout: 2000,
-      headers: { 'Authorization': 'JWT ' + localStorage.getItem('front_access_token') }
-   });
