42b1696af5dc66f165dd8392246b0a06c60ee3ed
Nirmit <42736478+nirmitwaghela@users.noreply.github.com>
2019-07-15 16:32:17 +0530

    delete old repo for the code cleanup process

--- nodeapi/config/keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://172.20.65.116/mernStack',
-  secretOrKey: 'mernStack'
-};
--- nodeapi/config/keys_prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://52.148.84.246/cooldex',
-  secretOrKey: 'cooldex'
-};
