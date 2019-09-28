ffeda75689aa471b1d43081cf8326f73972dda1c
PoojaSarvaiya <pooja@jbspl.com>
2019-09-28 11:14:55 +0530

    code cleanup
    old repo code

--- backoffice-app/src/selector/index.js
+++
@@ -1,9 +0,0 @@
-import { ServiceUtilConstant } from "../controllers/Constants";
-
-//Selector for TokenID
-//Used to send tokenId in all the request which are called after login
-export const userAccessToken = (state) => state.preference[ServiceUtilConstant.ACCESS_TOKEN];
-export const userRefreshToken = (state) => state.preference[ServiceUtilConstant.REFRESH_TOKEN];
-
-//Selector for getting user detail
-export const userDetail = (state) => state.EditProfileReducer.data;
